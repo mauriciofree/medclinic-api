c55ebd7e4fcfcae4ea2ad83438bb8b54830aaf65
mauriciofree <mauricio@freeline.inf.br>
2026-09-13 12:08:39 -0300

    feat: cria rota de login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
 import { CreateUserDTO } from "../dtos/CreateUserDTO";
+import { LoginDTO } from "../dtos/LoginDTO";
 import { asyncHandler } from "../middlewares/asyncHandler";
 import { validateDto } from "../middlewares/validateDto";
 
@@ -12,4 +13,10 @@ authRoutes.post(
   "/register",
   validateDto(CreateUserDTO),
   asyncHandler((request, response) => authController.register(request, response))
+);
+
+authRoutes.post(
+  "/login",
+  validateDto(LoginDTO),
+  asyncHandler((request, response) => authController.login(request, response))
 );
