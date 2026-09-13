797bd34201112820341269654ba7f75e988f170f
mauriciofree <mauricio@freeline.inf.br>
2026-09-13 11:04:01 -0300

    feat: configura servidor express

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+export const routes = Router();
+
+routes.get("/", (request, response) => {
+  return response.json({
+    message: "MedClinic API",
+    status: "online"
+  });
+});
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import { routes } from "./routes";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
